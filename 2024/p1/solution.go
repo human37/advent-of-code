1d3f48e53420a6a86b0ed20dfc1895d55cf7dfa7
Ammon Taylor <ammonx9@gmail.com>
2024-11-30 22:33:31 -0700

    2024 d1 p2

--- 2024/p1/solution.go
+++ 2024/p1/solution.go
@@ -26,7 +26,7 @@ func readInput(path string) ([]int, []int, error) {
 		if err != nil {
 			return nil, nil, err
 		}
-		rightNum, err := strconv.Atoi(parts[1]) 
+		rightNum, err := strconv.Atoi(parts[1])
 		if err != nil {
 			return nil, nil, err
 		}
@@ -36,38 +36,28 @@ func readInput(path string) ([]int, []int, error) {
 	return left, right, nil
 }
 
-func sort(arr []int) []int {
-	sorted := make([]int, len(arr))
-	copy(sorted, arr)
-	for i := 0; i < len(sorted)-1; i++ {
-		for j := 0; j < len(sorted)-i-1; j++ {
-			if sorted[j] > sorted[j+1] {
-				sorted[j], sorted[j+1] = sorted[j+1], sorted[j]
-			}
-		}
-	}
-	return sorted
-}
-
-func abs(a int, b int) int {
-	if a > b {
-		return a - b
-	}
-	return b - a
-}
-
 func solve(left []int, right []int) int {
-	leftSorted := sort(left)
-	rightSorted := sort(right)
 
-	var distances []int
-	for i := 0; i < len(leftSorted); i++ {
-		distances = append(distances, abs(leftSorted[i], rightSorted[i]))
+	rightMap := make(map[int]int)
+	for i := 0; i < len(right); i++ {
+		if _, ok := rightMap[right[i]]; !ok {
+			rightMap[right[i]] = 0
+		}
+		rightMap[right[i]]++
+	}
+
+	var similarities []int
+	for i := 0; i < len(left); i++ {
+		if _, ok := rightMap[left[i]]; ok {
+			similarities = append(similarities, left[i]*rightMap[left[i]])
+		} else {
+			similarities = append(similarities, 0)
+		}
 	}
 
 	sum := 0
-	for _, distance := range distances {
-		sum += distance
+	for _, similarity := range similarities {
+		sum += similarity
 	}
 
 	return sum
@@ -75,7 +65,7 @@ func solve(left []int, right []int) int {
 }
 
 func main() {
-	left, right, err := readInput("example-input.txt")
+	left, right, err := readInput("input.txt")
 	if err != nil {
 		panic(err)
 	}
