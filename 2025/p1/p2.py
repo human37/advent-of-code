6f0e1cfc19a3b66baadedb00e29a49e94e7c5b59
Ammon Taylor <ammonx9@gmail.com>
2025-12-02 16:49:52 -0700

    added 2025 p1 solution

---
+++ 2025/p1/p2.py
@@ -0,0 +1,52 @@
+input_file = "input.txt"
+position = 50
+answer = 0
+
+def parse_input():
+    input = []
+    with open(input_file) as f:
+        for line in f:
+            if line != "":
+                input.append(line.strip())
+    return input
+
+def solution_p2(input):
+    global position, answer
+
+    for rotation in input:
+        if rotation[0] == "R":
+            rotate_right(int(rotation[1:]))
+        elif rotation[0]== "L":
+            rotate_left(int(rotation[1:]))
+    print("solution part 2:", answer)
+
+def rotate_left(distance):
+    global position, answer
+    for rotation in range(distance):
+        if position == 0:
+            position = 99
+        else:
+            position -= 1
+        if position == 0:
+            answer += 1;
+
+def rotate_right(distance):
+    global position, answer
+    for rotation in range(distance):
+        if position == 99:
+            position = 0
+        else:
+            position += 1
+        if position == 0:
+            answer += 1
+
+
+def main():
+    input = parse_input()
+    solution_p2(input)
+
+
+
+if __name__ == "__main__":
+    main();
+
